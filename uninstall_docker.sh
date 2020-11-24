3289ae249026cf086467a12d6e74356dc8b507a7
정재영 <43400316+J-jaeyoung@users.noreply.github.com>
2020-11-24 22:42:34 +0900

    Update uninstall_docker.sh

--- uninstall_docker.sh
+++ uninstall_docker.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 dpkg -l | grep -i docker
 
-sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+sudo apt-get purge -y docker-engine
+sudo apt-get purge -y docker 
+sudo apt-get purge -y docker.io
+sudo apt-get purge -y docker-ce
+sudo apt-get purge -y docker-ce-cli
 sudo apt-get autoremove -y --purge docker-engine
 sudo apt-get autoremove -y --purge docker
 sudo apt-get autoremove -y --purge docker.io
 sudo apt-get autoremove -y --purge docker-ce
 
-sudo rm -rf /var/lib/docker /etc/docker
+sudo rm -rf /var/lib/docker /etc/docker /var/run/docker
 sudo rm /etc/apparmor.d/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
